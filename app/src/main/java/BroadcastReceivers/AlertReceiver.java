423da0b8c27ff678ceb905e4710931a54c257626
Trinh Dung <c170017@e.ntu.edu.sg>
2019-08-01 21:22:33 +0800

    Basic alarm done

---
+++ app/src/main/java/BroadcastReceivers/AlertReceiver.java
@@ -0,0 +1,18 @@
+package BroadcastReceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import Utils.NotificationHelper;
+import androidx.core.app.NotificationCompat;
+
+public class AlertReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1,nb.build());
+    }
+}
